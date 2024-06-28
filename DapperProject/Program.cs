d71729eb9ef8358a6b7e35b7ab7355ef75a7a3c0
zuleyhaeker.16 <zuleyhaeker.16@gmail.com>
2024-06-28 21:50:28 +0300

    .

--- DapperProject/Program.cs
+++ DapperProject/Program.cs
@@ -1,7 +1,21 @@
+
+
+using DapperProject.Context;
+using DapperProject.Services.CategoryServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
+builder.Services.AddScoped<DapperContext>();
+
+
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+
+
+
 builder.Services.AddControllersWithViews();
+
 
 var app = builder.Build();
 
